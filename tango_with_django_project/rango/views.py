c17095ec69ffcc5275bbff2d6cbec222858ff1c0
arnabcsesust <arnabcsesust@gmail.com>
2014-04-12 21:17:35 +0600

    about view

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says Hello World")
+    return HttpResponse("Rango says Hello World <a href='/rango/about'>About</a>")
+
+def about(request):
+	return HttpResponse("Rango Says: Here is the about page <a href='/rango/'>Home</a>")
 
 
