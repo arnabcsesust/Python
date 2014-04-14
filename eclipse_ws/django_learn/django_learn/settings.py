83efb5af4ad97cbe9b8dee3d091932260e4b4e5a
arnabcsesust <arnabcsesust@gmail.com>
2014-04-15 00:55:17 +0600

    configure project with mysql database

--- eclipse_ws/django_learn/django_learn/settings.py
+++ eclipse_ws/django_learn/django_learn/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'django_learn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_learn',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
